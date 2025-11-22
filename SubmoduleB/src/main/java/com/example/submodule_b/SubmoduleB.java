7b97f0cae149807cbc5f76b4a6ef81717dfdcb00
vfyjxf_ <2331007009@qq.com>
2025-11-22 22:44:18 +0800

    fix reference

--- SubmoduleB/src/main/java/com/example/submodule_b/SubmoduleB.java
+++ SubmoduleB/src/main/java/com/example/submodule_b/SubmoduleB.java
@@ -1,16 +1,17 @@
 package com.example.submodule_b;
 
+import com.example.common.Common;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(SubmoduleA.MODID)
+@Mod(SubmoduleB.MODID)
 public class SubmoduleB {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "submodule_b";
 
-    public SubmoduleA(IEventBus modEventBus, ModContainer modContainer) {
+    public SubmoduleB(IEventBus modEventBus, ModContainer modContainer) {
         System.out.println(new Common());
     }
 
