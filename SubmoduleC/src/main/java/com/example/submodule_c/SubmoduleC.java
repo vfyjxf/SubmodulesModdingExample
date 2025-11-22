aa0a9e5b214138a835d7263cb33899dbc09c4ee3
vfyjxf_ <2331007009@qq.com>
2025-11-22 22:40:50 +0800

    fix bug

--- SubmoduleC/src/main/java/com/example/submodule_c/SubmoduleC.java
+++ SubmoduleC/src/main/java/com/example/submodule_c/SubmoduleC.java
@@ -1,16 +1,17 @@
-package com.example.examplemod;
+package com.example.submodule_c;
 
+import com.example.common.Common;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(SubmoduleA.MODID)
-public class SubmoduleA {
+@Mod(SubmoduleC.MODID)
+public class SubmoduleC {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "submodule_a";
+    public static final String MODID = "submodule_c";
 
-    public SubmoduleA(IEventBus modEventBus, ModContainer modContainer) {
+    public SubmoduleC(IEventBus modEventBus, ModContainer modContainer) {
         System.out.println(new Common());
     }
 
